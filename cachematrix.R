3ab6c22a3935125b217bc602b8a0117a6bb28673
knnkv <knnkv@users.noreply.github.com>
2016-07-06 04:25:32 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For the purpose of saving the computational resources and time
+## we want to store the results of our calculations in cache
+## so that we don't need to recompute the values every time
+## we need to access these values.
+## This is especially crucial when calculations are complex
+## and require significant time to be completed
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a list with 4 functions:
+## 1. set(x) assigns a new matrix
+## 2. get() returns the matrix from cache
+## 3. setinv(x) assigns the solved matrix
+## 4. getinv() returns the solved matrix from cache
+ 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xinv <- NULL
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) xinv <<- inv
+  getinv <- function() xinv
+  list(set=set,get=get,
+       setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that 'solves' the matrix from the previous function or inverts it.
+## It checks whether the inverted matrix already exists: if yes, it simply returns it from cache
+## if not - solves the matrix and saves the result in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getinv()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  data <- x$get()
+  xinv <- solve(data)
+  x$setinv(xinv)
+  xinv
 }
